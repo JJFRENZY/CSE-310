725ed58695a1c7fee0ff7d9f68e7fff5af5aee6a
JJFRENZY <jackjayeck@icloud.com>
2026-01-10 02:15:53 -0700

    week_1

---
+++ Hello-World/main.py
@@ -0,0 +1,8 @@
+
+## Video Demonstration
+# A video demonstration has been created that includes my face, a demonstration of the program running, and a walkthrough of the code.
+
+# Video Link: https://youtu.be/PASTE_YOUR_VIDEO_LINK_HERE
+
+## GitHub Repository
+# https://github.com/JJFRENZY/CSE-310
