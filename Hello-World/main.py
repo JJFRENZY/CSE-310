9b8da0c885001380c3d715a18da9bc30c3dec725
JJFRENZY <jackjayeck@icloud.com>
2026-01-10 02:21:33 -0700

    week01

--- Hello-World/main.py
+++ Hello-World/main.py
@@ -1,8 +1,6 @@
+def main():
+    print("Hello World")
 
-## Video Demonstration
-# A video demonstration has been created that includes my face, a demonstration of the program running, and a walkthrough of the code.
 
-# Video Link: https://youtu.be/PASTE_YOUR_VIDEO_LINK_HERE
-
-## GitHub Repository
-# https://github.com/JJFRENZY/CSE-310
+if __name__ == "__main__":
+    main()
